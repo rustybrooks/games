1dded6badaebd3810ecc01a536b9404d7108e5d5
Rusty Brooks <rbrooks@alienvault.com>
2021-12-13 16:29:55 -0600

    basics work

--- src/ui/webpack.config.js
+++ src/ui/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: "development",
+  devtool: 'inline-source-map',
   entry: './src/index.tsx',
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
@@ -17,4 +27,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Hot Module Replacement',
+       filename: "index.html",
+       hash: true,
+       template: "src/index.html",
+     }),
+   ],
+   devServer: {
+     // static: './public',
+    hot: true,
+   },
 };
