fce71ee32a8ec92bcab46b0a97c5e53d4f5c5bed
Rusty Brooks <rbrooks@alienvault.com>
2021-12-13 18:36:09 -0600

    stuff kinda works

--- src/ui/src/components/Worldle.tsx
+++ src/ui/src/components/Worldle.tsx
@@ -14,6 +14,10 @@ const style = () => {
     button: {
       padding: 5,
     },
+
+    divContainer: {},
+
+    pContainer: {},
   };
   x.pressBlank = { ...x.button, ['background-color']: '#eee' };
   x.press = { ...x.button, ['background-color']: '#66F' };
@@ -26,14 +30,6 @@ const style = () => {
 };
 
 const Wordle = ({ classes }: { classes: { [id: string]: any } }) => {
-  const onChange = (input: any) => {
-    console.log('Input changed', input);
-  };
-
-  const onKeyPress = (button: any) => {
-    console.log('Button pressed', button);
-  };
-
   const init_grid = () => {
     let igrid = [];
     for (let y = 0; y < 9; y++) {
@@ -47,43 +43,82 @@ const Wordle = ({ classes }: { classes: { [id: string]: any } }) => {
   };
 
   let [grid, setGrid] = React.useState(() => init_grid());
+  const [inputs, setInputs] = React.useState({});
+  const [layoutName, setLayoutName] = React.useState('default');
+  const [inputName, setInputName] = React.useState('default');
+  const keyboard = React.useRef(null);
+
+  const onChangeAll = (inputs: any) => {
+    /**
+     * Here we spread the inputs into a new object
+     * If we modify the same object, react will not trigger a re-render
+     */
+    setInputs({ ...inputs });
+    console.log('Inputs changed', inputs);
+  };
+
+  const onKeyPress = (button: any) => {
+    console.log('Button pressed', button);
+  };
+
+  const onChangeInput = (event: any) => {
+    const inputVal = event.target.value;
+
+    setInputs({
+      ...inputs,
+      [inputName]: inputVal,
+    });
+
+    keyboard.current.setInput(inputVal);
+  };
+
+  const getInputValue = (inputName: string) => {
+    console.log(inputName);
+    // return inputs[inputName] || '';
+  };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '90%',
-      }}
-    >
-      <table>
-        <tbody>
-          {[0, 1, 2, 3, 4].map(y => {
-            return (
-              <tr key={y}>
-                {[0, 1, 2, 3, 4].map(x => {
-                  if (y === 0 && x === 8) {
-                    return <td key={x}>&nbsp;</td>;
-                  } else {
-                    const g = grid[y][x];
-                    const c1 = g.selected ? 'select' : 'selectBlank';
-                    const c2 = g.pressed ? (g.error ? 'pressError' : 'press') : 'pressBlank';
-                    return (
-                      <td key={x} className={classes[c1]}>
-                        <div className={classes[c2]}>
-                          {y}, {x}
-                        </div>
-                      </td>
-                    );
-                  }
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Keyboard onChange={onChange} onKeyPress={onKeyPress} />
+    <div style={{ height: '100%', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: 500, padding: 20 }}>
+        <table style={{ margin: '0 auto', padding: 20 }}>
+          <tbody>
+            {[0, 1, 2, 3, 4].map(y => {
+              return (
+                <tr key={y}>
+                  {[0, 1, 2, 3, 4].map(x => {
+                    if (y === 0 && x === 8) {
+                      return <td key={x}>&nbsp;</td>;
+                    } else {
+                      const g = grid[y][x];
+                      const c1 = g.selected ? 'select' : 'selectBlank';
+                      const c2 = g.pressed ? (g.error ? 'pressError' : 'press') : 'pressBlank';
+                      return (
+                        <td key={x} className={classes[c1]}>
+                          <div className={classes[c2]}>
+                            {y}, {x}
+                          </div>
+                        </td>
+                      );
+                    }
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div style={{ width: 500 }}>
+          <Keyboard
+            layout={{
+              default: ['q w e r t y u i o p {bksp}', 'a s d f g h j k l', 'z x c v b n m'],
+            }}
+            keyboardRef={r => (keyboard.current = r)}
+            inputName={inputName}
+            layoutName={layoutName}
+            onChangeAll={onChangeAll}
+            onKeyPress={onKeyPress}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/ui/src/index.tsx
+++ src/ui/src/index.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { withStore, useGetAndSet } from 'react-context-hook'
+import { withStore, useGetAndSet } from 'react-context-hook';
 
 // import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles';
@@ -13,60 +13,49 @@ import Tab from '@material-ui/core/Tab';
 
 import Wordle from './components/Worldle';
 
-
 const styles = {
-    root: {
-        // flexGrow: 1,
-        height: "100%",
-    },
-    flex: {
-        flexGrow: 1,
-    },
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 0,
+    paddingLeft: 0,
+  },
 
-    tabLink : {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        'padding-right': 0,
-        'padding-left': 0,
-    }
+  tabLink: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 0,
+    paddingLeft: 0,
+  },
 };
 
-const NavBarX = ({classes} : any) => {
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Tabs value={0}>
-                    <Tab
-                        key='Home'
-                        label='home'
-                        className={classes.tabLink}
-                    />
-                </Tabs>
-            </AppBar>
-            <Wordle/>
-        </div>
-    )
-}
+const NavBarX = ({ classes }: any) => {
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={0}>
+          <Tab key="Home" label="home" className={classes.tabLink} />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+};
 
 const initialValue = {
-  'midiCallbackMap': {},
-  'midiInputs': {},
-}
+  midiCallbackMap: {},
+  midiInputs: {},
+};
 
-const NavBar = withStore(withStyles(styles)(NavBarX), initialValue)
-
+const NavBar = withStore(withStyles(styles)(NavBarX), initialValue);
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavBar/>} />
-      </Routes>
-    </BrowserRouter>,
-    document.getElementById("root")
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Wordle />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
-
-
-
-
-
