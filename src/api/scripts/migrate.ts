c5c84f3b9a9aa0e3efe147a5d59f9e592f3fec1f
Rusty Brooks <me@rustybrooks.com>
2021-12-18 15:30:37 -0600

    Added some more migrations

--- src/api/scripts/migrate.ts
+++ src/api/scripts/migrate.ts
@@ -25,8 +25,9 @@ initial.addStatement('create index users_api_key on lower(users(api_key))');
 initial.addStatement(`
     create table leagues(
         league_id serial primary key,
-        league_name varchar(200),
-        created timestamp
+        league_name varchar(200) not null, 
+        created timestamp not null default now(),
+        rules json
     )
 `);
 initial.addStatement('create index leagues_league_name on leagues(league_name)');
@@ -34,8 +35,32 @@ initial.addStatement('create index leagues_league_name on leagues(league_name)')
 initial.addStatement(`
     create table league_members(
         league_member_id serial primary key,
-        user_id bigint references users(user_id),
-        league_id bigint references leagues(league_id)
+        user_id bigint not null references users(user_id),
+        league_id bigint not null references leagues(league_id),
+        added timestamp not null default now()
+    )
+`);
+
+initial.addStatement(`
+    create table answer(
+        answer_id serial primary key,
+        league_id bigint not null references leagues(league_id),
+        answer varchar(10) not null,
+        created timestamp not null default now(),
+        active_after not null timestamp,
+        active_before timestamp
+    )
+`);
+
+initial.addStatement(`
+    create table guesses(
+        guess_id serial primary key,
+        user_id bigint not null references users(user_id),
+        league_id bigint not null references leagues(league_id),
+        guess varchar(10) not null,
+        correct_placement smallint not null,
+        correct_letter smallint not null,
+        created timestamp not null default now()               
     )
 `);
 
@@ -54,12 +79,12 @@ const bootstrapAdmin = async () => {
   }
 };
 
-const migrate = async ({ apply, initial }: { apply: number[]; initial: boolean }) => {
+const migrate = async ({ apply, isInitial }: { apply: number[]; isInitial: boolean }) => {
   const SQL = pgexplorer.sql.sqlFactory({ writeUrl: process.env.WRITE_URL });
-  console.log('apply', apply, 'initial', initial);
-  const val = await migrations.Migration.migrate(SQL, initial, apply);
+  console.log('apply', apply, 'initial', isInitial);
+  const val = await migrations.Migration.migrate(SQL, isInitial, apply);
   console.log(val);
   await bootstrapAdmin();
 };
 
-migrate({ apply: [], initial: false }).then(() => process.exit(0));
+migrate({ apply: [], isInitial: false }).then(() => process.exit(0));
